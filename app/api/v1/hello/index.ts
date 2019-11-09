1c17cfe3755e4b2f812947aeca3c31f6eea3ae1f
Sumeet Bansal <sumeetbansal@gmail.com>
2019-11-08 22:48:29 -0800

    temp

--- app/api/v1/hello/index.ts
+++ app/api/v1/hello/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 
-export const router = express.Router();
+const router = express.Router();
 
 router.route('/')
   .get((req: express.Request, res: express.Response) => {
     res.json({ hello: 'world' });
   });
+
+module.exports = { router };
